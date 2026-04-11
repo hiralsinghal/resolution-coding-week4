c0e57af3d55a65a24055db323b6468320f6a77de
hiralsinghal <singhal.hiral@gmail.com>
2026-04-11 12:20:28 +0530

    made it ig

--- calc.c
+++ calc.c
@@ -3,14 +3,47 @@
 
 int main() {
     double a, b, answer;
+    char op;
+    const char *error = NULL;
+
     printf("Enter first number:\n");
     scanf("%lf", &a);
 
     printf("Enter second number:\n");
     scanf("%lf", &b);
 
-    answer = a + b;
-    printf("Answer: %lf", answer);
+    
+
+    printf("Enter operator:\n");
+    scanf(" %c", &op);
+
+    if (op == "+") {
+        answer = a + b;
+    }
+
+    else if (op == "-") {
+        answer = a - b;
+    }
+
+    else if (op == "*") {
+        answer = a*b;
+    }
+
+    else if (op == "/") {
+        answer = a/b;
+    }
+
+    else {
+        error = "Unknown operator";
+    }
+
+    if (error) {
+        printf(error);
+    }
+    
+    else {
+        printf("Answer: %lf", answer);
+    }
 
     return 0;
 }
