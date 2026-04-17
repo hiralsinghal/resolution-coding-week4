4e4a851dbaddd4a6cf7d66edbbcb8e76acf55142
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-17 22:23:02 +0530

    done

--- counter.c
+++ counter.c
@@ -1,2 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define LINE_LENGTH 1024
+
+int main() {
+    FILE *filePointer = fopen("repeated.txt", "r");
+
+    char buffer[LINE_LENGTH];
+    int lines = 0, words = 0;
+    char lastChar = ' ';
+
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
+        lines++;
+
+        for (size_t i = 0; buffer[i] != '\0'; i++) {
+            const char current = buffer[i];
+
+            if ((current == ' ' || current == '\n') && lastChar != ' ' && lastChar != '\n') {
+                words++;
+            }
+
+            lastChar = current;
+        }
+    }
+
+    if (lastChar != ' ' && lastChar != '\n') {
+        words++;
+    }
+
+    printf("Total lines: %d\nTotal words: %d", lines, words);
+
+    return 0;
+}
